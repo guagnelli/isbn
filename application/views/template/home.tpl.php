b504ba46d6d65de33c169c4b96b198749117486d
leas88 <cenitluis.pumas@gmail.com>
2016-10-31 01:27:43 -0600

    detalle

--- application/views/template/home.tpl.php
+++ application/views/template/home.tpl.php
@@ -20,6 +20,7 @@
 		<?php echo css("style.css"); ?>
 		<?php echo css("font-awesome.css"); ?>
 		<?php echo css("nprogress.css"); ?>
+		<?php echo css("style-isbn.css"); ?>
 		<?php //echo css("bootstrap-datetimepicker.css"); ?>
 
 		<!-- Custom and plugin javascript -->
