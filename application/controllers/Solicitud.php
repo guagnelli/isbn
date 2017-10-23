b40d94023c045fdad7a7a527b745da3b0773431a
Miguel Guagnelli <guagnelli@Miguels-MacBook-Pro.local>
2017-10-23 09:09:55 -0500

    Prueba utf8_encode

--- application/controllers/Solicitud.php
+++ application/controllers/Solicitud.php
@@ -372,7 +372,7 @@ class Solicitud extends MY_Controller {
                         $dgaj = $this->req->get_usuario(array("rol_cve" => E_rol::DGAJ, "usu_estado" => 1), 'usu_nombre as nombre, usu_correo as correo');
                         $solicitud_datos['solicitud'] = $this->req->getSolicitud($solicitud);
                         $solicitud_datos['historial'] = $this->req->getHistorial($solicitud);
-                        $envio = $this->correo->enviar_correo(array('subject' => utf8_encode('Notificación de actividad en el Sistema ISBN-UNAM'),
+                        $envio = $this->correo->enviar_correo(array('subject' => utf8_decode('Notificación de actividad en el Sistema ISBN-UNAM'),
                             'body' => $this->load->view('solicitud/correo/plantilla.php', $solicitud_datos, TRUE),
                             'addAddress' => array(array('correo' => $this->session->userdata('mail'), 'nombre' => $this->session->userdata('nombre'))),
                             'addCC' => $dgaj)
